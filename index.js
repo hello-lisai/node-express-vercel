462ed5a3a8f6a26bcc2462d81aba5c9f63768f05
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2023-12-05 04:14:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const { Client } = require('pg');
+ const { Client } = require('pg');
 
 const client = new Client({
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  host: process.env.POSTGRES_HOST,
+  user: 'default', 
+  password: '6KyZd2sNxTuv',
+  host: 'ep-odd-river-24248080-pooler.eu-central-1.postgres.vercel-storage.com',
   port: 5432,
-  database: process.env.POSTGRES_DATABASE,
+  database: 'verceldb',
 });
 
-client.connect()
-  .then(() => {
+async function connectAndInsertData() {
+  try {
+    await client.connect();
     console.log('连接成功');
-  })
-  .catch((err) => {
-    console.error('连接失败:', err);
-  });
+
+    // 在这里执行插入数据的操作
+    const query = 'INSERT INTO your_table(column1, column2) VALUES ($1, $2)'; 
+    const values = ['value1', 'value2'];
+    await client.query(query, values);
+    console.log('数据插入成功');
+
+    await client.end();
+    console.log('断开连接');
+  } catch (err) {
+    console.error('连接失败或数据插入失败:', err);
+  }
+}
+
+connectAndInsertData();
