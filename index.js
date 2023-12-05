15c403cdd7d3595d47bd7f19b0b066c2a56f512f
hello-lisai <105758032+hello-lisai@users.noreply.github.com>
2023-12-05 04:11:37 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-// Import packages
-const express = require("express");
-const home = require("./routes/home");
+const { Client } = require('pg');
 
-// Middlewares
-const app = express();
-app.use(express.json());
+const client = new Client({
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: 5432,
+  database: process.env.POSTGRES_DATABASE,
+});
 
-// Routes
-app.use("/home", home);
-
-// connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+client.connect()
+  .then(() => {
+    console.log('连接成功');
+  })
+  .catch((err) => {
+    console.error('连接失败:', err);
+  });
